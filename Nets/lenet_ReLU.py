651746540d357d23f3ca5e784ad5cb1c773ae158
Lucia Wuckert <maluwu@yahoo.com>
2020-10-09 15:40:41 +0200

    Added folder for brevitas-enabled nets

---
+++ Nets/lenet_ReLU.py
@@ -0,0 +1,31 @@
+# Following has been removed during forward pass between features and
+# classifiers:
+# x = self.avgpool(x)
+# x = torch.flatten(x, 1)
+
+import torch.nn as nn
+import brevitas.nn as qnn
+from brevitas.core.quant import QuantType
+
+class Net(nn.Module):
+            def __init__(self, num_classes, weight_quant_type, weight_bit_width, quant_type, bit_width):
+                super(Net, self).__init__()
+                self.features = nn.Sequential(
+                    qnn.QuantConv2d(1, 20, kernel_size=5, stride=1, padding=0,
+                        weight_quant_type, weight_bit_width),                   
+                    nn.MaxPool2d(kernel_size=2, stride=2),
+                    qnn.QuantConv2d(20, 50, kernel_size=5, padding=0,
+                        weight_quant_type, weight_bit_width),
+                    nn.MaxPool2d(kernel_size=2, stride=2),
+                    )
+                self.classifier = nn.Sequential(
+                    qnn.QuantLinear(64 * 50 * 4 * 4, 64 * 500, bias=True, 
+                        weight_quant_type=weightQuantType, weight_bit_width=weightBitWidth),
+                    qnn.QuantReLU(quant_type, bit_width)                    
+                    qnn.QuantLinear(64 * 500, 64 * 10, bias=False,
+                        weight_quant_type=weightQuantType, weight_bit_width=weightBitWidth),
+                    )
+            def forward(self, x):
+                x = self.features(x)
+                x = self.classifier(x)
+                return x
